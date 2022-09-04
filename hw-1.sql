23680a60787354e012c37486e110865b88a11dc3
никита осипов <nik.osipov2011@yandex.ru>
2022-09-04 12:12:19 +0300

    add homework

---
+++ hw-1.sql
@@ -0,0 +1,9 @@
+ /*1. Получите описание таблицы REGIONS, используя её полное имя.*/
+SELECT * FROM hr.regions;
+ /*2. Получите описание таблицы COUNTRIES, используя её полное имя.*/
+SELECT * FROM hr.countries;
+/*3. Получите описание таблицы JOBS, используя её короткое имя.*/
+SELECT * FROM hr.jobs;
+/*4. Получите описание таблицы LOCATIONS, используя её короткое имя.*/
+SELECT * FROM hr.locations;
+ 
