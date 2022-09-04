0b960e83af7194d6db4de79a08f5ce6a59c3aa38
никита осипов <nik.osipov2011@yandex.ru>
2022-09-04 13:30:36 +0300

    upd hw 3

--- hw-1.sql
+++ hw-1.sql
@@ -50,3 +50,70 @@ FROM dual;
 дней).*/
 SELECT 100*365.25*24*60 
 FROM dual;
+
+/*14.ѕолучите список всех сотрудников с именем David.*/
+SELECT * 
+FROM employees
+WHERE first_name = 'David';
+/*15. ѕолучите список всех сотрудников, у которых job_id равен FI_ACCOUNT.*/
+SELECT * 
+FROM employees
+WHERE job_id = 'FI_ACCOUNT';
+/*16.  ¬ыведите информацию о имени, фамилии, з/п и номере департамента 
+дл€ сотрудников из 50го департамента с зарплатой, большей 4000.*/
+SELECT first_name, last_name, salary, department_id
+FROM employees
+WHERE department_id = 50
+AND salary > 4000;
+/*17. ѕолучите список всех сотрудников, которые работают или в 20м, или в 
+30м департаменте.*/
+SELECT *
+FROM employees
+where department_id = 20 or department_id = 30;
+/*18. ѕолучите список всех сотрудников, у которых втора€ и последн€€ буква 
+в имени равна 'a'.*/
+SELECT *
+FROM employees
+WHERE first_name like '_a%a';
+/*19. ѕолучите список всех сотрудников из 50го и из 80го департамента, у 
+которых есть бонус(комиссионные). ќтсортируйте строки по email
+(возрастающий пор€док), использу€ его пор€дковый номер.*/
+SELECT *
+FROM employees
+WHERE (department_id = 50 or department_id = 80)
+AND commission_pct is not null
+order by 4;
+/*20. ѕолучите список всех сотрудников, у которых в имени содержатс€ 
+минимум 2 буквы 'n'.*/
+SELECT * 
+FROM employees
+WHERE first_name like '%n%n%';
+/*21. ѕолучить список всех сотрудников, у которых длина имени больше 4 
+букв. ќтсортируйте строки по номеру департамента (убывающий 
+пор€док) так, чтобы значени€ УnullФ были в самом конце.*/
+SELECT * 
+FROM employees
+WHERE first_name like '%_____%'
+order by department_id desc nulls last;
+/*22. ѕолучите список всех сотрудников, у которых зарплата находитс€ в 
+промежутке от 3000 до 7000 (включительно), нет бонуса 
+(комиссионных) и job_id среди следующих вариантов: PU_CLERK, 
+ST_MAN, ST_CLERK.*/
+SELECT * 
+FROM employees
+WHERE salary between 3000 and 7000
+AND commission_pct is null
+AND job_id in ('PU_CLERK', 'ST_MAN', 'ST_CLERK');
+/*23. ѕолучите список всех сотрудников у которых в имени содержитс€ 
+символ '%'.*/
+SELECT *
+FROM employees
+WHERE first_name like '%\%%' escape '\';
+/*24. ¬ыведите информацию о job_id, имене и з/п дл€ работников, рабочий 
+id которых больше или равен 120 и job_id не равен IT_PROG. 
+ќтсортируйте строки по job_id (возрастающий пор€док) и именам 
+(убывающий пор€док).*/
+SELECT job_id, first_name, salary
+FROM employees
+WHERE employee_id >= 120
+AND job_id != 'IT_PROG';
