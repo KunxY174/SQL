021709f119a1d89e9b70ff7dd4e2b1bfa54f3152
никита осипов <nik.osipov2011@yandex.ru>
2022-09-29 18:20:04 +0300

    upd sql

--- sql_academy.sql
+++ sql_academy.sql
@@ -275,3 +275,77 @@ on (s.id=sc.subject)
 join Teacher t
 on (sc.teacher=t.id)
 where last_name = 'Romashkin';
+
+--41.Во сколько начинается 4-ый учебный предмет по расписанию ?
+select start_pair
+from Timepair
+where id = 4;
+
+--42.Сколько времени обучающийся будет находиться в школе, учась со 2-го по 4-ый уч. предмет ?
+SELECT distinct TIMEDIFF ('11:50:00', '09:20:00') as time
+from Timepair;
+
+--43.Выведите фамилии преподавателей, которые ведут физическую культуру (Physical Culture). Отcортируйте преподавателей по фамилии.
+SELECT last_name
+from Teacher t
+  join Schedule s ON (t.id = s.teacher)
+where subject in (
+    select id
+    from subject
+    where name = 'Physical Culture')
+ORDER BY last_name;
+
+--44.Найдите максимальный возраст (колич. лет) среди обучающихся 10 классов ?
+select max(YEAR(now()) - YEAR(birthday)) as max_year
+from student s
+  join Student_in_class st on (s.id = st.student)
+where st.class in (
+    select id
+    from class
+    where name like '10 %');
+
+--45.Какой(ие) кабинет(ы) пользуются самым большим спросом?
+SELECT classroom
+FROM Schedule
+GROUP by classroom
+having count (classroom) = (
+    SELECT count (classroom)
+    from Schedule
+    group by classroom
+    ORDER by 1 desc
+    limit 1);
+
+--46.В каких классах введет занятия преподаватель "Krauze" ?
+SELECT DISTINCT name
+from teacher t
+  join Schedule s on (t.id = s.teacher)
+  join class c on (c.id = s.class)
+where last_name = 'Krauze';
+
+--47.Сколько занятий провел Krauze 30 августа 2019 г.?
+select count (subject) as count
+from Schedule s
+  join Teacher t on (s.teacher = t.id)
+where date = '2019-08-30'
+  and last_name = 'Krauze';
+
+--48.Выведите заполненность классов в порядке убывания
+select name,
+  count (student) as count
+from Class c
+  join Student_in_class st on (c.id = st.class)
+GROUP BY name
+order by count desc;
+
+--49.Какой процент обучающихся учится в 10 A классе ?
+SELECT (count(student) * 100 / (select count(student)
+                                from Student_in_class)) as percent
+from Class c
+  join Student_in_class st on (c.id = st.class)
+where name = '10 A';
+
+--50.Какой процент обучающихся родился в 2000 году? Результат округлить до целого в меньшую сторону.
+SELECT FLOOR((COUNT(id) * 100 /(SELECT COUNT(id)
+                                from Student))) as percent
+FROM Student
+WHERE YEAR(birthday) = '2000';
