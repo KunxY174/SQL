0bf5ada45eafef358d4c8cc8798fe8184c64603a
никита осипов <nik.osipov2011@yandex.ru>
2022-10-05 19:16:07 +0300

    abc

--- sql_academy.sql
+++ sql_academy.sql
@@ -461,13 +461,37 @@ on (re.reservation_id=r.id)
 GROUP by room_id 
 
 --66.Вывести список комнат со всеми удобствами (наличие ТВ, интернета, кухни и кондиционера), а также общее количество дней и сумму за все дни аренды каждой из таких комнат.
+SELECT home_type, address, 
+ifnull (sum(DATEDIFF (end_date, start_date)), 0) as days,
+ifnull (sum(total), 0) as total_fee
+from Rooms r
+LEFT JOIN Reservations re
+on(r.id=re.room_id)
+where has_tv = 1
+and has_internet = 1
+and has_kitchen = 1
+and has_air_con = 1
+GROUP by r.id;
 
 --67.Вывести время отлета и время прилета для каждого перелета в формате "ЧЧ:ММ, ДД.ММ - ЧЧ:ММ, ДД.ММ", где часы и минуты с ведущим нулем, а день и месяц без.
 
 --68.Для каждой комнаты, которую снимали как минимум 1 раз, найдите имя человека, снимавшего ее последний раз, и дату, когда он выехал
 
 --69.Вывести идентификаторы всех владельцев комнат, что размещены на сервисе бронирования жилья и сумму, которую они заработали
+SELECT owner_id, ifnull (sum(re.total), 0) as total_earn
+from Rooms r
+left join Reservations re
+on (r.id=re.room_id )
+group by owner_id;
 
 --70.Необходимо категоризовать жилье на economy, comfort, premium по цене соответственно <= 100, 100 < цена < 200, >= 200. В качестве результата вывести таблицу с названием категории и количеством жилья, попадающего в данную категорию
+select (case
+when price <=100 then 'economy'
+when price >100
+and price <200 then 'comfort'
+when price >=200 then 'premium'
+end ) as category, COUNT(id) as count
+from Rooms
+group by category;
 
 --71.Найдите какой процент пользователей, зарегистрированных на сервисе бронирования, хоть раз арендовали или сдавали в аренду жилье. Результат округлите до сотых.
