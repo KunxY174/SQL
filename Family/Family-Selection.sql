eeef05969b885e883d3b4b5343a9856979c51981
никита осипов <nik.osipov2011@yandex.ru>
2022-11-12 10:29:10 +0300

    upd

--- Family/Family-Selection.sql
+++ Family/Family-Selection.sql
@@ -1,4 +1,4 @@
---17.Определить, сколько потратил в 2005 году каждый из членов семьи
+--1.Определить, сколько потратил в 2005 году каждый из членов семьи
 SELECT member_name,
   status,
   sum(unit_price * amount) as costs
@@ -11,21 +11,21 @@ where payment_id in (
   )
 group by member_name, status
 
---18.Узнать, кто старше всех в семьe
+--2.Узнать, кто старше всех в семьe
 SELECT member_name
 from FamilyMembers
 where birthday in (
     SELECT min(birthday)
     FROM FamilyMembers);
 
---19.Определить, кто из членов семьи покупал картошку (potato)
+--3.Определить, кто из членов семьи покупал картошку (potato)
 SELECT distinct status
 from FamilyMembers f
   join Payments p on (f.member_id = p.family_member)
   join Goods g on (p.good = g.good_id)
 where good_name = 'potato';
 
---20.Сколько и кто из семьи потратил на развлечения (entertainment). Вывести статус в семье, имя, сумму
+--4.Сколько и кто из семьи потратил на развлечения (entertainment). Вывести статус в семье, имя, сумму
 SELECT status,
   member_name,
   sum(amount * unit_price) as costs
@@ -36,19 +36,19 @@ from FamilyMembers f
 where good_type_name = 'entertainment'
 GROUP by status, member_name;
 
---21.Определить товары, которые покупали более 1 раза
+--5.Определить товары, которые покупали более 1 раза
 select distinct good_name
 from Goods g
   join Payments p on (g.good_id = p.good)
 GROUP by good_name
 HAVING count (amount) > 1;
 
---22.Найти имена всех матерей (mother)
+--6.Найти имена всех матерей (mother)
 select member_name
 from FamilyMembers
 where status = 'mother';
 
---23.Найдите самый дорогой деликатес (delicacies) и выведите его стоимость
+--7.Найдите самый дорогой деликатес (delicacies) и выведите его стоимость
 SELECT good_name,
   unit_price
 from Goods g
@@ -62,7 +62,7 @@ where good_type_name = 'delicacies'
       join GoodTypes gt on (g.type = gt.good_type_id)
     where good_type_name = 'delicacies');
 
---24.Определить кто и сколько потратил в июне 2005
+--8.Определить кто и сколько потратил в июне 2005
 SELECT member_name,
   sum(unit_price * amount) as costs
 from Payments p
@@ -73,7 +73,7 @@ WHERE payment_id in (
     where DATE_FORMAT(date, '%b-%Y') = 'Jun-2005')
 GROUP by member_name;
 
---25.Определить, какие товары не покупались в 2005 году
+--9.Определить, какие товары не покупались в 2005 году
 select good_name
 from Goods
 where good_id not in (
@@ -81,7 +81,7 @@ where good_id not in (
     from Payments
     where YEAR(date) = 2005);
 
---26.Определить группы товаров, которые не приобретались в 2005 году
+--10.Определить группы товаров, которые не приобретались в 2005 году
 SELECT distinct good_type_name
 from GoodTypes gt
 WHERE good_type_id not in (
@@ -90,7 +90,7 @@ WHERE good_type_id not in (
       join Payments p on (g.good_id = p.good)
     where YEAR(date) = 2005);
 
---27.Узнать, сколько потрачено на каждую из групп товаров в 2005 году. Вывести название группы и сумму
+--11.Узнать, сколько потрачено на каждую из групп товаров в 2005 году. Вывести название группы и сумму
 SELECT good_type_name,
   sum(unit_price * amount) as costs
 from Payments p
@@ -99,7 +99,7 @@ from Payments p
 WHERE YEAR(date) = 2005
 group by good_type_name;
 
---29.Выведите имена пассажиров улетевших в Москву (Moscow) на самолете TU-134
+--12.Выведите имена пассажиров улетевших в Москву (Moscow) на самолете TU-134
 SELECT distinct name
 from passenger p
   join Pass_in_trip pt on (p.id = pt.passenger)
@@ -109,16 +109,16 @@ where trip in (
     where plane = 'TU-134'
       and town_to = 'Moscow');
 
---31.Вывести всех членов семьи с фамилией Quincey.
+--13.Вывести всех членов семьи с фамилией Quincey.
 select *
 from FamilyMembers
 where member_name like '% Quincey';
 
---32.Вывести средний возраст людей (в годах), хранящихся в базе данных. Результат округлите до целого в меньшую сторону.
+--14.Вывести средний возраст людей (в годах), хранящихся в базе данных. Результат округлите до целого в меньшую сторону.
 SELECT FLOOR (avg(TIMESTAMPDIFF(YEAR, birthday, now()))) as age
 from FamilyMembers;
 
---33.Найдите среднюю стоимость икры. В базе данных хранятся данные о покупках красной (red caviar) и черной икры (black caviar).
+--15.Найдите среднюю стоимость икры. В базе данных хранятся данные о покупках красной (red caviar) и черной икры (black caviar).
 SELECT avg(unit_price) as cost
 from Payments p
   join Goods g on (p.good = g.good_id)
--- Schedule/Schedule-Selection.sql
+++ Schedule/Schedule-Selection.sql
@@ -1,34 +1,34 @@
---34.Сколько всего 10-ых классов
+--1.Сколько всего 10-ых классов
 select count(name) as count
 from class
 where name like '10 %';
 
---35.Сколько различных кабинетов школы использовались 2.09.2019 в образовательных целях ?
+--2.Сколько различных кабинетов школы использовались 2.09.2019 в образовательных целях ?
 SELECT count(classroom) as count
 from Schedule
 where date = '2019-09-02';
 
---36.Выведите информацию об обучающихся живущих на улице Пушкина (ul. Pushkina)?
+--3.Выведите информацию об обучающихся живущих на улице Пушкина (ul. Pushkina)?
 select *
 from student
 WHERE address like 'ul. Pushkina%';
 
---37.Сколько лет самому молодому обучающемуся ?
+--4.Сколько лет самому молодому обучающемуся ?
 SELECT TIMESTAMPDIFF (YEAR, max(birthday), now()) as year
 from student;
 
---38.Сколько Анн (Anna) учится в школе ?
+--5.Сколько Анн (Anna) учится в школе ?
 SELECT count(id) as count
 FROM Student
 where first_name = 'Anna';
 
---39.Сколько обучающихся в 10 B классе ?
+--6.Сколько обучающихся в 10 B классе ?
 select count(student) as count
 from Student_in_class s
   join class c on (s.class = c.id)
 where name like '10 B';
 
---40.Выведите название предметов, которые преподает Ромашкин П.П. (Romashkin P.P.) ?
+--7.Выведите название предметов, которые преподает Ромашкин П.П. (Romashkin P.P.) ?
 select name as subjects
 from subject s  
 join Schedule sc
@@ -37,16 +37,16 @@ join Teacher t
 on (sc.teacher=t.id)
 where last_name = 'Romashkin';
 
---41.Во сколько начинается 4-ый учебный предмет по расписанию ?
+--8.Во сколько начинается 4-ый учебный предмет по расписанию ?
 select start_pair
 from Timepair
 where id = 4;
 
---42.Сколько времени обучающийся будет находиться в школе, учась со 2-го по 4-ый уч. предмет ?
+--9.Сколько времени обучающийся будет находиться в школе, учась со 2-го по 4-ый уч. предмет ?
 SELECT distinct TIMEDIFF ('11:50:00', '09:20:00') as time
 from Timepair;
 
---43.Выведите фамилии преподавателей, которые ведут физическую культуру (Physical Culture). Отcортируйте преподавателей по фамилии.
+--10.Выведите фамилии преподавателей, которые ведут физическую культуру (Physical Culture). Отcортируйте преподавателей по фамилии.
 SELECT last_name
 from Teacher t
   join Schedule s ON (t.id = s.teacher)
@@ -56,7 +56,7 @@ where subject in (
     where name = 'Physical Culture')
 ORDER BY last_name;
 
---44.Найдите максимальный возраст (колич. лет) среди обучающихся 10 классов ?
+--11.Найдите максимальный возраст (колич. лет) среди обучающихся 10 классов ?
 select max(YEAR(now()) - YEAR(birthday)) as max_year
 from student s
   join Student_in_class st on (s.id = st.student)
@@ -65,7 +65,7 @@ where st.class in (
     from class
     where name like '10 %');
 
---45.Какой(ие) кабинет(ы) пользуются самым большим спросом?
+--12.Какой(ие) кабинет(ы) пользуются самым большим спросом?
 SELECT classroom
 FROM Schedule
 GROUP by classroom
@@ -76,21 +76,21 @@ having count (classroom) = (
     ORDER by 1 desc
     limit 1);
 
---46.В каких классах введет занятия преподаватель "Krauze" ?
+--13.В каких классах введет занятия преподаватель "Krauze" ?
 SELECT DISTINCT name
 from teacher t
   join Schedule s on (t.id = s.teacher)
   join class c on (c.id = s.class)
 where last_name = 'Krauze';
 
---47.Сколько занятий провел Krauze 30 августа 2019 г.?
+--14.Сколько занятий провел Krauze 30 августа 2019 г.?
 select count (subject) as count
 from Schedule s
   join Teacher t on (s.teacher = t.id)
 where date = '2019-08-30'
   and last_name = 'Krauze';
 
---48.Выведите заполненность классов в порядке убывания
+--15.Выведите заполненность классов в порядке убывания
 select name,
   count (student) as count
 from Class c
@@ -98,20 +98,20 @@ from Class c
 GROUP BY name
 order by count desc;
 
---49.Какой процент обучающихся учится в 10 A классе ?
+--16.Какой процент обучающихся учится в 10 A классе ?
 SELECT (count(student) * 100 / (select count(student)
                                 from Student_in_class)) as percent
 from Class c
   join Student_in_class st on (c.id = st.class)
 where name = '10 A';
 
---50.Какой процент обучающихся родился в 2000 году? Результат округлить до целого в меньшую сторону.
+--17.Какой процент обучающихся родился в 2000 году? Результат округлить до целого в меньшую сторону.
 SELECT FLOOR((COUNT(id) * 100 /(SELECT COUNT(id)
                                 from Student))) as percent
 FROM Student
 WHERE YEAR(birthday) = '2000';
 
---63.Выведите отсортированный список (по возрастанию) имен студентов в виде Фамилия.И.О.
+--18.Выведите отсортированный список (по возрастанию) имен студентов в виде Фамилия.И.О.
 select concat (last_name, '.',
 SUBSTRING(first_name, 1, 1), '.', 
 SUBSTRING(middle_name, 1, 1), '.') as name
